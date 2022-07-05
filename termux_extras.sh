cea085f8af33a2608b90ca7030a2dfb45a8295f1
huytuph <89845294+huytuph@users.noreply.github.com>
2022-07-05 13:29:21 +1000

    Update termux_extras.sh

--- termux_extras.sh
+++ termux_extras.sh
@@ -1,1 +1,29 @@
+#!/bin/bash/
 
+# make script executable
+# chmod +x termux_extras.sh
+
+# to run script
+# ./termux_extras.sh
+
+pkgup = pkg update - y && pkg upgrade -y
+
+complete () {
+echo "
+complete
+"
+}
+
+$pkgup
+complete
+$pkg install git -y
+complete
+$pkg install vim -y
+complete
+$pkg install python -y
+complete
+pkg install nodejs-lts -y
+complete
+
+$pkgup
+complete
